9e169118877b635b7a97ee0d102a01e2cd883ca5
abb909 <abbsalh30@gmail.com>
2025-07-19 18:41:12 +0100

    Fix Admin component default export

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <p>Admin functionality coming soon...</p>
+    </div>
+  );
+};
+
+export default Admin;
